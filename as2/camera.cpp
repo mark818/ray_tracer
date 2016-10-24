00294e8a6a4afcc1a2cc786c511244dd8228eab1
Chuhan Z <cookiechu@users.noreply.github.com>
2016-10-23 23:29:17 -0700

    Bugless curently

--- as2/camera.cpp
+++ as2/camera.cpp
@@ -8,8 +8,8 @@ camera::camera(vec3 eye, vec3 ll, vec3 lr, vec3 ul, vec3 ur)
   }
 
 ray camera::generate_ray(double u, double v) const {
-  vec3 point = ll + u * (lr - ll) + v * (ul - ll);
-    //(1-u)*(v*ll + (1-v)*ul) + u*(v*lr + (1-v)*ur);
+  // vec3 point = ll + u * (lr - ll) + v * (ul - ll);
+  vec3 point = (1-u)*(v*ll + (1-v)*ul) + u*(v*lr + (1-v)*ur);
   return ray(eye, (point - eye).unit());
 }
 
